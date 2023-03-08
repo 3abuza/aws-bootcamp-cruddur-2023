3e5b45f69d8fc7e7398be38ba3f0ee42c8482244
3abuza Victor <olajumoketoby03@gmail.com>
2023-03-08 14:22:09 +0000

    add xray

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -34,7 +34,7 @@ provider.add_span_processor(processor)
 
 # X-RAY --------
 xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 XRayMiddleware(app, xray_recorder)
 
 # Show this in the logs within the backend-flask app (STDOUT)
